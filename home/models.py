ca1c1512285bb7a915f4fd1589b2c9f547f4c930
mikhail.trunov <mikhail.trunov@gmail.com>
2020-02-28 10:41:27 +0400

    Menu model

--- home/models.py
+++ home/models.py
@@ -3,8 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class aboutpost(models.Model):
+class LeftMenu(models.Model):
+    menuName = models.CharField(max_length=30)
+
+class SitePost(models.Model):
     postTitle = models.CharField(max_length=30)
     postName = models.TextField()
     postText = models.TextField()
     postUrl = models.CharField(max_length=30)
+    LastUpdated = models.DateField()
+    leftMenu = models.ForeignKey(LeftMenu, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return postTitle
