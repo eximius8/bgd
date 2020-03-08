f1623a6d135fba0a4afb647636439c85922c23f3
Mikhail Trunov <mikhail.trunov@gmail.com>
2020-03-08 09:00:12 +0000

    Commit from pythonanywhere

--- bgd/settings.py
+++ bgd/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gd8^v&m86hcz)k6w=8zom2+#aazqn^+clvfz^wca7jb!f%&_*e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eximius8.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
